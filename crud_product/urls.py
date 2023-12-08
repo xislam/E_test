38875bceb110c65959e54f78f46e332a3e7be1b9
Islam <50731444+xislam@users.noreply.github.com>
2023-12-08 13:50:54 +0600

    Создал ProductListCreateView  ProductDetailView для него  serializers Ты обновил урлы

---
+++ crud_product/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .api import ProductListCreateView, ProductDetailView
+
+urlpatterns = [
+    path('products/', ProductListCreateView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
+]
--- root/asgi.py
+++ root/asgi.py
@@ -6,11 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import notifications.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": URLRouter(
+        notifications.routing.websocket_urlpatterns
+    ),
+})

0248d7367f3d8935fb9a1f464d166488d052ddbc
Islam <50731444+xislam@users.noreply.github.com>
2023-12-08 13:56:00 +0600

    Обновил добавил нотификацию уведомление при действии CRUD

--- crud_product/api.py
+++ crud_product/api.py
@@ -1,4 +1,7 @@
-from rest_framework import generics
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+from rest_framework import generics, status
+from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -7,7 +10,59 @@ class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
+    def create(self, request, *args, **kwargs):
+        # Логика создания объекта Product
+
+        # Отправка уведомления
+        self.send_notification('Новый продукт добавлен!')
+
+        return Response({'message': 'Product created successfully'}, status=status.HTTP_201_CREATED)
+
+    def send_notification(self, message):
+        channel_layer = get_channel_layer()
+
+        async def send():
+            await channel_layer.group_send(
+                'notification_group',
+                {
+                    'type': 'send_notification',
+                    'message': message,
+                }
+            )
+
+        async_to_sync(send)()
+
 
 class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def update(self, request, *args, **kwargs):
+        # Логика обновления объекта Product
+
+        # Отправка уведомления
+        self.send_notification('Продукт обновлен!')
+
+        return Response({'message': 'Product updated successfully'}, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        # Логика удаления объекта Product
+
+        # Отправка уведомления
+        self.send_notification('Продукт удален!')
+
+        return Response({'message': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+    def send_notification(self, message):
+        channel_layer = get_channel_layer()
+
+        async def send():
+            await channel_layer.group_send(
+                'notification_group',
+                {
+                    'type': 'send_notification',
+                    'message': message,
+                }
+            )
+
+        async_to_sync(send)()
--- crud_user/api.py
+++ crud_user/api.py
@@ -1,1 +1,58 @@
+from rest_framework import generics
+from .models import User
+from .serializers import UserSerializer
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
 
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        self.send_notification('Новый пользователь добавлен!')
+        return response
+
+    def send_notification(self, message):
+        channel_layer = get_channel_layer()
+
+        async def send():
+            await channel_layer.group_send(
+                'notification_group',
+                {
+                    'type': 'send_notification',
+                    'message': message,
+                }
+            )
+
+        async_to_sync(send)()
+
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+        self.send_notification('Пользователь обновлен!')
+        return response
+
+    def destroy(self, request, *args, **kwargs):
+        response = super().destroy(request, *args, **kwargs)
+        self.send_notification('Пользователь удален!')
+        return response
+
+    def send_notification(self, message):
+        channel_layer = get_channel_layer()
+
+        async def send():
+            await channel_layer.group_send(
+                'notification_group',
+                {
+                    'type': 'send_notification',
+                    'message': message,
+                }
+            )
+
+        async_to_sync(send)()
